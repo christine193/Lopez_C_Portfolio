e6602d6b3da4984fca79680b1675c399f701d61c
christine193 <35804233+christine193@users.noreply.github.com>
2018-10-26 10:31:43 -0400

    structure

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('animatedSkull'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+  
+  })
+ 
