80914d7d9245b71e0e081c3af99b1ee78301f22e
christine193 <35804233+christine193@users.noreply.github.com>
2018-11-19 20:39:06 -0500

    responsive and a load more button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 //instagram feed
+var loadMoreButton = document.getElementById("loadMoreButton");
+
 var feed = new Instafeed({
     get:"user",
+    limit:6,
     userId: "8699890662",
     accessToken:'8699890662.1677ed0.988f17324a344ad8935e86aea0f10a0a',
     resolution:"low_resolution",
-    template: '<a href="{{link}}"><img class="instaImage" src="{{image}}" /></a>',
+    template: '<a href="{{link}}"><img class="instaImage" src="{{image}}"></a>',
  
+    onSuccess: function() {
+        if(!this.hasNext()) loadMoreButton.disabled = true;
+    }
+});
+    loadMoreButton.addEventListener("click", function() {
+    feed.next();
 });
 feed.run();
 
